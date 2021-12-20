ea4542f9cdc7547ff8ba023e867c346bf2235a70
Jafeth8 <romeojafet@gmail.com>
2021-12-19 22:22:44 -0600

    ejercicio con useEffect

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { CounterWithCustomHook } from './components/01-useState/CounterWithCustomHook';
+import { SimpleForm } from './components/02-useEffect/SimpleForm';
+//import { CounterWithCustomHook } from './components/01-useState/CounterWithCustomHook';
 //import { CounterApp } from './components/01-useState/CounterApp';
 
 //import './index.css';
@@ -9,7 +10,7 @@ import { CounterWithCustomHook } from './components/01-useState/CounterWithCusto
 //import { HookApp } from './HookApp';
 
 ReactDOM.render(
-    <CounterWithCustomHook/>,
+    <SimpleForm/>,
   document.getElementById('root')
 );
 
