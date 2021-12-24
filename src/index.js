4db81af60688bc93218609c7dc36ba20c38aaa4d
Jafeth8 <romeojafet@gmail.com>
2021-12-24 16:42:26 -0600

    useRef

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 //import { FormWithCustomHook } from './components/02-useEffect/FormWithCustomHook';
-import { MultipleCustomHooks } from './components/03-examples/MultipleCustomHooks';
+//import { MultipleCustomHooks } from './components/03-examples/MultipleCustomHooks';
 //import { SimpleForm } from './components/02-useEffect/SimpleForm';
 //import { CounterWithCustomHook } from './components/01-useState/CounterWithCustomHook';
 //import { CounterApp } from './components/01-useState/CounterApp';
+//import { FocusScreen } from './components/04-useRef/FocusScreen';
+import { RealExampleRef } from './components/04-useRef/RealExampleRef';
 
 //import './index.css';
 //import App from './App';
@@ -12,7 +14,7 @@ import { MultipleCustomHooks } from './components/03-examples/MultipleCustomHook
 //import { HookApp } from './HookApp';
 
 ReactDOM.render(
-    <MultipleCustomHooks/>,
+    <RealExampleRef/>,
   document.getElementById('root')
 );
 
